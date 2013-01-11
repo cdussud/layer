efebe6b40b3abb7ef94d863f144ef7c244ea25c9
Cedric Dussud <cdussud@hotmail.com>
2013-01-11 10:27:36 -0500

    DockPanel

--- app.js
+++ app.js
@@ -13,13 +13,23 @@
 // Doesn't work. Part of layout, and we've turned it off with absolute. 
 // My panel's overflow property is now useless
 
-
-
-// Next:
-
 // clipping -- for the elment that's absolutely positioned
 // apply clip to child if necessary. clip is relative to child's space.
 // $child.css('clip', 'rect(0px,60px,200px,0px)');
+
+
+// Alternative
+// Just set proper css classes. 
+// Can this all be done in CSS with no JS? What's the benefit then?
+
+
+// Next:
+// Think of a real-world case. Create a panel to support it
+// - Blog layout with header, content, sidebar, and footer
+// - Mini layout of form items
+
+// Other panels
+// pinterest / masonry: http://masonry.desandro.com/
 
 
 
@@ -32,7 +42,112 @@
 // - define vertical alignment somehow
 // - handle clipping / scrolling
 
+  var Rect = (function(){
+    function Rect(x, y, width, height){
+      // TODO: default to 0
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+    }
+
+    return Rect;
+  })();
+
+function setOuterWidth(element, width){
+  element.width(width - (element.outerWidth(true) - element.width()))
+}
+
+function setOuterHeight(element, height){
+  element.height(height - (element.outerHeight(true) - element.height()))
+}
+
+
+
 (function(){
+
+
+  var DockPanel = (function(){
+
+    function DockPanel(){
+      this.$el = $('#container');   // borrow from backbone
+    }
+
+    DockPanel.prototype.children = function(){
+      return this.$el.children();
+    };
+
+    DockPanel.prototype.layout = function() {
+
+      // TODO: what if children don't specify how they dock?
+      // TODO: deal with what happens when we run out of space.
+      // TODO: implement bottom and right
+      
+      var numChildren = this.children().length;
+      var contentWidth = this.$el.width();
+      var contentHeight = this.$el.height();
+
+      var contentArea = new Rect(0, 0, contentWidth, contentHeight);
+
+      this.children().each(function(index, item) {
+
+        var $child = $(item);
+        var width = $child.outerWidth(true);
+        var height = $child.outerHeight(true);
+
+        var offsetX = 0;
+        var offsetY = 0;
+
+        if ($child.hasClass('dock-top')){
+
+          setOuterWidth($child, contentArea.width);
+          offsetX = contentArea.x;
+          offsetY = contentArea.y;
+
+          contentArea.y += height;
+          contentArea.height -= height;
+        }
+        else if ($child.hasClass('dock-left')){
+
+          setOuterHeight($child, contentArea.height);
+          offsetX = contentArea.x;
+          offsetY = contentArea.y;
+
+          contentArea.x += width;
+          contentArea.width -= width;
+        }
+        else if ($child.hasClass('dock-right')){
+
+        }
+        else if ($child.hasClass('dock-bottom')){
+          
+        }
+        else if (index + 1 == numChildren){
+          // last child with nothing specified fills the remaining space
+          setOuterWidth($child, contentArea.width);
+          setOuterHeight($child, contentArea.height);
+          offsetX = contentArea.x;
+          offsetY = contentArea.y;
+
+          contentArea.width = 0;
+          contentArea.height = 0;
+        }
+
+
+        // Lay out the children
+        $child.css('position', 'absolute');
+        $child.css('transform','translate(' + offsetX + 'px, ' + offsetY + 'px)');
+
+      });
+
+      // TODO: panel has to size properly... fix this later
+      this.$el.width(contentWidth);
+      this.$el.height(contentHeight);
+    };
+
+    return DockPanel;
+
+  })();
 
   var WrapPanel = (function(){
 
@@ -42,10 +157,6 @@
 
     WrapPanel.prototype.children = function(){
       return this.$el.children();
-    };
-
-    WrapPanel.prototype.setOffset = function (child, x, y){
-
     };
 
     WrapPanel.prototype.layout = function() {
@@ -133,7 +244,7 @@
   var x;
   $(function(){
   //  x = new StackPanel(true);
-    x = new WrapPanel();
+    x = new DockPanel();
     x.layout();
   });
 
